02d0aabfc53f24ac0a41aca94b55f0d134de8a06
Vijay Pratap Singh <58662665+vijaypratap12@users.noreply.github.com>
2023-03-09 20:49:57 -0800

    Added while loop

---
+++ ConsoleApp/ControlStatements.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class ControlStatements
+    {
+        public int ConditionOne()
+        {
+            int a = 10;
+            int b = 30;
+            while(a-- == 9)
+            {
+                return a;
+            }
+            return a + b;
+        }
+    }
+}
--- ConsoleApp/RefType.cs
+++ ConsoleApp/RefType.cs
@@ -12,9 +12,28 @@ namespace ConsoleApp
     }
     public class RefType
     {
+        string emp3 = "Uhtred";
+        string emp4 = "Demon";
+
+        //constructor
+        public RefType(string emp1, string emp2)
+        {
+            emp1 = emp3;
+            emp2 = emp4;    
+        }
+        public string Employee()
+        {
+            return emp3+emp4;
+        }
+        //method overloading
+        public int Employee(int a, int b)
+        {
+            return a + b;
+        }
         static void Main(string[] args)
         {
             User user = new User();
+            RefType refType = new RefType("Baba", "Yaga");
             User user2 = user;
             Console.WriteLine(user2.Name);
             Console.WriteLine(user.Name);
@@ -23,6 +42,17 @@ namespace ConsoleApp
             user2.Name = "Vijay";
             Console.WriteLine(user2.Name);
             Console.WriteLine(user.Name);
+
+            string name = refType.Employee();
+            int val = refType.Employee(10, 20);
+            Console.WriteLine(name);
+            Console.WriteLine(val);
+
+
+            //control statements
+            ControlStatements control = new ControlStatements();
+            int result = control.ConditionOne();
+            Console.WriteLine(result);
             Console.ReadLine();
         }
     }
