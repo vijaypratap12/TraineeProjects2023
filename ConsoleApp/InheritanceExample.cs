87acf527c61ec09c14f6bf45477c88ba4b2dee4f
Vijay Pratap Singh <vijaypratap12121999@gmail.com>
2023-03-10 15:14:22 +0530

    Added OOPs Examples

---
+++ ConsoleApp/InheritanceExample.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class InheritanceExample
+    {
+    }
+    public class Employee
+    {
+        public void AddEmployee()
+        {
+            Console.WriteLine("This will add employee");
+        }
+
+    }
+    public class Company:Employee
+    {
+        public void ShowEmployee()
+        {
+            Console.WriteLine("This is show employee");
+        }
+    }
+//    sealed class Vehicle
+//    {
+//}
+
+//    class Car : Vehicle
+//    {
+//}
+}
