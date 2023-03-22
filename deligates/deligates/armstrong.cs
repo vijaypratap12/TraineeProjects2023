a71d250bbb10d0a7b1823ef7a612ae73144a2961
Priyanshu Agnihotri <priyanshu.agn@gmail.com>
2023-03-22 17:04:41 +0530

    added new files of delegates

---
+++ deligates/deligates/armstrong.cs
@@ -0,0 +1,42 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace deligates
+{
+    public delegate void armstrong(int x);
+    public class arm
+    {
+        public void armstrong(int x)
+        {
+            int n, m=0, temp;
+            temp = x;
+            while(x>0)
+            {
+                n = x % 10;
+                m = m + (n * n * n);
+                x = x / 10;
+            }
+            if(m==temp)
+            {
+                Console.WriteLine("its a armstrong number");
+            }
+            else
+            {
+                Console.WriteLine("Not a armstrong number");
+            }
+        }
+        public static void Main(string[] args) 
+        { 
+            arm a =new arm();
+            Console.WriteLine("Enter the number");
+            int x= int.Parse(Console.ReadLine());
+            armstrong am = new armstrong(a.armstrong);
+            am(x);
+            Console.ReadLine();
+        }  
+
+    }
+}*/
---
+++ deligates/deligates/palindrome.cs
@@ -0,0 +1,45 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace deligates
+{
+    public delegate void palindrome(int x);
+    public class pm
+    {
+        public void palindrome(int x)
+        {
+            int n, temp,rev=0;
+            temp = x;
+            while(x>0)
+            {
+                n = x % 10;
+                rev = rev * 10 + n ;
+                x = x / 10;
+            }
+            if(rev==temp)
+            {
+                Console.WriteLine("palindrome number");
+            }
+            else
+            {
+                Console.WriteLine("NOT a palindrome number");
+
+            }
+           
+        }
+        public static void Main(string[] args) 
+        {
+            pm p=new pm();
+            Console.WriteLine("enter the number");
+            int x = int .Parse(Console.ReadLine());
+            palindrome pr = new palindrome(p.palindrome);
+            pr(x);
+            Console.ReadLine();
+
+
+        }
+    }
+}*/
